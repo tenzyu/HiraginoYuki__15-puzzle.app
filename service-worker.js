fc5c07ff33b2236fc834a4ba4193e78476adeb2e
tenzyu <tenzyu.on@gmail.com>
2020-12-21 19:05:24 +0900

    Updates

--- service-worker.js
+++ service-worker.js
@@ -1,39 +1,25 @@
-/**
- * Welcome to your Workbox-powered service worker!
- *
- * You'll need to register this file in your web app and you should
- * disable HTTP caching for this file too.
- * See https://goo.gl/nhQhGp
- *
- * The rest of the code is auto-generated. Please don't update this file
- * directly; instead, make changes to your Workbox build configuration
- * and re-run your build process.
- * See https://goo.gl/2aRDsh
- */
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.0.2/workbox-sw.js');
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+ workbox.routing.registerRoute(
+   ({ request }) => request.mode === 'navigate',
+   new workbox.strategies.NetworkFirst({
+     cacheName: 'pages',
+   }),
+ );
 
-importScripts(
-  "/precache-manifest.87f56f292fa43634615d30553c2bc161.js"
-);
+ workbox.routing.registerRoute(
+   ({ request }) =>
+     request.destination === 'style' ||
+     request.destination === 'script' ||
+     request.destination === 'worker',
+   new workbox.strategies.NetworkFirst({
+     cacheName: 'assets',
+   }),
+ );
 
-self.addEventListener('message', (event) => {
-  if (event.data && event.data.type === 'SKIP_WAITING') {
-    self.skipWaiting();
-  }
-});
-
-workbox.core.clientsClaim();
-
-/**
- * The workboxSW.precacheAndRoute() method efficiently caches and responds to
- * requests for URLs in the manifest.
- * See https://goo.gl/S9QRab
- */
-self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
-
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
-  
-  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
-});
+ workbox.routing.registerRoute(
+   ({ request }) => request.destination === 'image',
+   new workbox.strategies.CacheFirst({
+     cacheName: 'images',
+   }),
+ ); 
